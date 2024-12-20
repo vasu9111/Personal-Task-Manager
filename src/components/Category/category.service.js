ce52024f7bcebe5d514bd2a8ab35507def182d17
vasu9111 <vasugondaliya1510@gamil.com>
2024-12-20 11:14:40 +0530

    feat/category-get
    - User all Category show.
    - user category show by userid.

--- src/components/Category/category.service.js
+++ src/components/Category/category.service.js
@@ -1,4 +1,5 @@
 import categoryMld from "../../models/cetegory.js";
+// cerate a categoey add
 const createCategory = async (userId, categoryData) => {
   try {
     const category = new categoryMld({
@@ -15,7 +16,21 @@ const createCategory = async (userId, categoryData) => {
     throw error;
   }
 };
-
+// get user all category
+const getAllCategory = async (userId) => {
+  try {
+    const category = await categoryMld.find({ userId });
+    if (category.length === 0) {
+      const error = new Error("CATEGORY_NOT_FOUND");
+      throw error;
+    }
+    return category;
+  } catch (err) {
+    const error = new Error(err.message);
+    throw error;
+  }
+};
 export default {
   createCategory,
+  getAllCategory,
 };
